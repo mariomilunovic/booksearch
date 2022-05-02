3a3ac548efbd0a4adcac35a1f12999e1be8c9ad9
Mario Milunovic <mario.milunovic@gmail.com>
2022-05-02 16:54:53 +0200

    fixing app env

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,4 +1,4 @@
 <div class="p-4 font-bold text-white text-shadow-md">
     © 2022 Copyright:
-    <a class="text-reset font-bold text-yellow-300  text-shadow-md" href="https://www.booksearch.gq/">booksearch.gq</a>
+    <a class="text-reset font-bold text-yellow-300  text-shadow-md" href="https://booksearch.gq/">booksearch.gq</a>
 </div>
